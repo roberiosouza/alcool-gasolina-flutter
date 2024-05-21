3f5017003aec8f5de32aaf094a3157154a423ffb
Robério <rroberiosouza@gmail.com>
2024-05-20 21:37:41 -0300

    Criação de interface com estilo

--- lib/alcool_gasolina.dart
+++ lib/alcool_gasolina.dart
@@ -11,8 +11,67 @@ class _AlcoolGasolinaState extends State<AlcoolGasolina> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: Padding(padding: EdgeInsets.all(32)),
+      appBar: AppBar(
+        title: Text(
+          "Álcool ou Gasolina",
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: Colors.lightBlueAccent,
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(32),
+        child: Column(
+          children: [
+            Image.asset("images/logo.png"),
+            Padding(
+              padding: EdgeInsets.only(top: 30),
+              child: Text(
+                "Saiba qual melhor opção para abastecimento do seu carro",
+                style: TextStyle(
+                  fontSize: 22,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: 15),
+              child: TextField(
+                keyboardType: TextInputType.number,
+                decoration:
+                    InputDecoration(labelText: "Preço Álcool, ex: 1.59"),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: 15),
+              child: TextField(
+                keyboardType: TextInputType.number,
+                decoration:
+                    InputDecoration(labelText: "Preço Gasolina, ex: 3.15"),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: 15),
+              child: SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  child: Text(
+                    "Calcular",
+                    style: TextStyle(
+                        fontSize: 15,
+                        fontWeight: FontWeight.w800,
+                        color: Colors.white),
+                  ),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.lightBlueAccent,
+                    shape: ContinuousRectangleBorder(),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
