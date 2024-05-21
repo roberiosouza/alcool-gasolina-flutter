0a49038c56b2663ac69e8ae7fae63782830991b4
Robério <rroberiosouza@gmail.com>
2024-05-20 21:58:26 -0300

    Ajustes nas posições e fontes dos widgets4

--- lib/alcool_gasolina.dart
+++ lib/alcool_gasolina.dart
@@ -8,6 +8,9 @@ class AlcoolGasolina extends StatefulWidget {
 }
 
 class _AlcoolGasolinaState extends State<AlcoolGasolina> {
+  TextEditingController _alcoolController = TextEditingController();
+  TextEditingController _gasolinaController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,58 +21,69 @@ class _AlcoolGasolinaState extends State<AlcoolGasolina> {
         ),
         backgroundColor: Colors.lightBlueAccent,
       ),
-      body: Padding(
-        padding: EdgeInsets.all(32),
-        child: Column(
-          children: [
-            Image.asset("images/logo.png"),
-            Padding(
-              padding: EdgeInsets.only(top: 30),
-              child: Text(
-                "Saiba qual melhor opção para abastecimento do seu carro",
-                style: TextStyle(
-                  fontSize: 22,
-                  fontWeight: FontWeight.bold,
+      body: Container(
+        child: SingleChildScrollView(
+          padding: EdgeInsets.all(32),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Image.asset("images/logo.png"),
+              Padding(
+                padding: EdgeInsets.only(top: 30),
+                child: Text(
+                  "Saiba qual melhor opção para abastecimento do seu carro",
+                  style: TextStyle(
+                    fontSize: 22,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
               ),
-            ),
-            Padding(
-              padding: EdgeInsets.only(top: 15),
-              child: TextField(
-                keyboardType: TextInputType.number,
-                decoration:
-                    InputDecoration(labelText: "Preço Álcool, ex: 1.59"),
+              Padding(
+                padding: EdgeInsets.only(top: 15),
+                child: TextField(
+                  keyboardType: TextInputType.number,
+                  decoration:
+                      InputDecoration(labelText: "Preço Álcool, ex: 1.59"),
+                  controller: _alcoolController,
+                ),
               ),
-            ),
-            Padding(
-              padding: EdgeInsets.only(top: 15),
-              child: TextField(
-                keyboardType: TextInputType.number,
-                decoration:
-                    InputDecoration(labelText: "Preço Gasolina, ex: 3.15"),
+              Padding(
+                padding: EdgeInsets.only(top: 15),
+                child: TextField(
+                  keyboardType: TextInputType.number,
+                  decoration:
+                      InputDecoration(labelText: "Preço Gasolina, ex: 3.15"),
+                  controller: _gasolinaController,
+                ),
               ),
-            ),
-            Padding(
-              padding: EdgeInsets.only(top: 15),
-              child: SizedBox(
-                width: double.infinity,
-                child: ElevatedButton(
-                  onPressed: () {},
+              Padding(
+                padding: EdgeInsets.only(top: 15),
+                child: SizedBox(
+                  width: double.infinity,
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    child: Text(
+                      "Calcular",
+                      style: TextStyle(
+                          fontSize: 15,
+                          fontWeight: FontWeight.w800,
+                          color: Colors.white),
+                    ),
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.lightBlueAccent,
+                      shape: ContinuousRectangleBorder(),
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                  padding: EdgeInsets.only(top: 20),
                   child: Text(
-                    "Calcular",
-                    style: TextStyle(
-                        fontSize: 15,
-                        fontWeight: FontWeight.w800,
-                        color: Colors.white),
-                  ),
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.lightBlueAccent,
-                    shape: ContinuousRectangleBorder(),
-                  ),
-                ),
-              ),
-            ),
-          ],
+                    "Resultado",
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+                  )),
+            ],
+          ),
         ),
       ),
     );
